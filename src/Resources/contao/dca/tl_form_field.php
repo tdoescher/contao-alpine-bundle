8e6c8d6684475bbff0aa32992e3e0af3326ff4d9
Torben Döscher <mail@tdoescher.de>
2025-04-01 18:19:48 +0200

    x-model is only set automatically if ##model_object## is used in the form, Bugfixes

--- src/Resources/contao/dca/tl_form_field.php
+++ src/Resources/contao/dca/tl_form_field.php
@@ -4,21 +4,21 @@ $GLOBALS['TL_DCA']['tl_form_field']['fields']['xInit'] = [
     'exclude' => true,
     'inputType' => 'text',
     'eval' => [ 'rte' => 'ace|js', 'tl_class' => 'w50' ],
-    'sql' => [ 'type' => 'text', 'default' => '' ]
+    'sql' => [ 'type' => 'text', 'notnull' => false ]
 ];
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['xShow'] = [
     'exclude' => true,
     'inputType' => 'text',
     'eval' => [ 'rte' => 'ace|js', 'tl_class' => 'w50' ],
-    'sql' => [ 'type' => 'text', 'default' => '' ]
+    'sql' => [ 'type' => 'text', 'notnull' => false ]
 ];
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['xClass'] = [
     'exclude' => true,
     'inputType' => 'text',
     'eval' => [ 'rte' => 'ace|js', 'tl_class' => ' clr 100' ],
-    'sql' => [ 'type' => 'text', 'default' => '' ]
+    'sql' => [ 'type' => 'text', 'notnull' => false ]
 ];
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['xModel'] = [
@@ -32,54 +32,54 @@ $GLOBALS['TL_DCA']['tl_form_field']['fields']['xOnChange'] = [
     'exclude' => true,
     'inputType' => 'text',
     'eval' => [ 'rte' => 'ace|js', 'tl_class' => 'clr w50' ],
-    'sql' => [ 'type' => 'text', 'default' => '' ]
+    'sql' => [ 'type' => 'text', 'notnull' => false ]
 ];
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['xOnInput'] = [
     'exclude' => true,
     'inputType' => 'text',
     'eval' => [ 'rte' => 'ace|js', 'tl_class' => 'w50' ],
-    'sql' => [ 'type' => 'text', 'default' => '' ]
+    'sql' => [ 'type' => 'text', 'notnull' => false ]
 ];
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['xOnClick'] = [
     'exclude' => true,
     'inputType' => 'text',
     'eval' => [ 'rte' => 'ace|js', 'tl_class' => 'clr w100' ],
-    'sql' => [ 'type' => 'text', 'default' => '' ]
+    'sql' => [ 'type' => 'text', 'notnull' => false ]
 ];
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['xOnFocus'] = [
     'exclude' => true,
     'inputType' => 'text',
     'eval' => [ 'rte' => 'ace|js', 'tl_class' => 'clr w50' ],
-    'sql' => [ 'type' => 'text', 'default' => '' ]
+    'sql' => [ 'type' => 'text', 'notnull' => false ]
 ];
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['xOnBlur'] = [
     'exclude' => true,
     'inputType' => 'text',
     'eval' => [ 'rte' => 'ace|js', 'tl_class' => 'w50' ],
-    'sql' => [ 'type' => 'text', 'default' => '' ]
+    'sql' => [ 'type' => 'text', 'notnull' => false ]
 ];
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['xBindClass'] = [
     'exclude' => true,
     'inputType' => 'text',
     'eval' => [ 'rte' => 'ace|js', 'tl_class' => 'clr w100' ],
-    'sql' => [ 'type' => 'text', 'default' => '' ]
+    'sql' => [ 'type' => 'text', 'notnull' => false ]
 ];
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['xBindDisabled'] = [
     'exclude' => true,
     'inputType' => 'text',
     'eval' => [ 'rte' => 'ace|js', 'tl_class' => 'clr w50' ],
-    'sql' => [ 'type' => 'text', 'default' => '' ]
+    'sql' => [ 'type' => 'text', 'notnull' => false ]
 ];
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['xBindRequired'] = [
     'exclude' => true,
     'inputType' => 'text',
     'eval' => [ 'rte' => 'ace|js', 'tl_class' => 'w50' ],
-    'sql' => [ 'type' => 'text', 'default' => '' ]
+    'sql' => [ 'type' => 'text', 'notnull' => false ]
 ];
